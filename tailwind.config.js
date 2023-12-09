6c2f34f53136fedb128ecc1316e5d2f9c1426015
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-12-09 20:31:55 +0500

    second page initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      // sans: ['Oswald', 'sans-serif'],
+      // sans: ['Bebas Neue', 'sans-serif'],
+      // sans: ['Anton', 'sans-serif'],
+      'anton': ['Anton', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
